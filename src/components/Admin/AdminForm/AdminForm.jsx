dc8eb18621fe3a4681405a741d90a9d06cb0d492
Ezequiel Amin <ezequielamin@outlook.com>
2023-10-12 16:22:30 -0300

    termino seccion de la lectura de blogs

--- src/components/Admin/AdminForm/AdminForm.jsx
+++ src/components/Admin/AdminForm/AdminForm.jsx
@@ -18,8 +18,6 @@ const AdminForm = (props) => {
   } = useForm();
 
   const handleSubmit = (data) => {
-    console.log(data);
-
     const newBlog = { ...data, id: generateId() };
     setBlogs((prev) => [...prev, newBlog]);
 
