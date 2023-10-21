2c513a70431c146736ea89f397948f5e0d08a1eb
Ezequiel Amin <ezequielamin@outlook.com>
2023-10-21 14:59:45 -0300

    agrego pagina de home y de error

--- src/views/ErrorView.jsx
+++ src/views/ErrorView.jsx
@@ -1,6 +1,18 @@
+import { Link } from 'react-router-dom';
+
 const ErrorView = () => {
   return (
-    <div>ErrorView</div>
-  )
-}
-export default ErrorView
+    <>
+      <h3 className='text-center mt-5'>Oh oh. No encontramos este recurso!</h3>
+      <p className='text-center'>
+        Por favor, revisa la URL o intenta nuevamente
+      </p>
+      <div className='text-center'>
+        <Link to={-1} className='btn btn-dark mt-3'>
+          Volver
+        </Link>
+      </div>
+    </>
+  );
+};
+export default ErrorView;
